2d0a397a8939a74875b96db8d68708dad320c2dc
Alegna Medina <public.alegna.21@gmail.com>
2021-06-18 12:28:52 -0400

    Player and Enemy Highlighted when card is chosen.

--- Life Out There/Assets/Scripts/DragDrop.cs
+++ Life Out There/Assets/Scripts/DragDrop.cs
@@ -6,10 +6,22 @@ using UnityEngine;
 public class DragDrop : MonoBehaviour
 {
     private bool isSelected = false;
-    
+    private bool isplayerOn = false;
+    private bool isenemyOn = false;
+
     public GameObject arrowEmitter;
     public GameObject cardHighlight;
 
+    private GameObject player;
+    private GameObject enemy;
+    GameObject enemyLight;
+    GameObject playerLight;
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+
+        enemy = GameObject.FindWithTag("Enemy");
+    }
 
     // Update is called once per frame
     void Update()
@@ -31,13 +43,40 @@ public class DragDrop : MonoBehaviour
             isSelected = true;
             transform.position = new Vector2(transform.position.x, transform.position.y + 50);
             cardHighlight.SetActive(true);
-            Debug.Log("This object is selected and its position is <" + transform.position + ">");
+            // Debug.Log("This object is selected and its position is <" + transform.position + ">");
+
+            if (this.name == "AttackCard(Clone)")
+            {
+                enemyLight = enemy.transform.GetChild(0).gameObject;
+                enemyLight.SetActive(true);
+                isenemyOn = true;
+                Debug.Log("This object is <" + this.name + "> and its highlight is <" + enemyLight.name + ">");
+            }
+            else if (this.name == "BlockCard(Clone)")
+            {
+                playerLight = player.transform.GetChild(0).gameObject;
+                playerLight.SetActive(true);
+                isplayerOn = true;
+                Debug.Log("This object is <" + this.name + "> and its highlight is <" + playerLight.name + ">");
+            }
+
         }
         else
         {
             isSelected = false;
             cardHighlight.SetActive(false);
             transform.position = new Vector2(transform.position.x, transform.position.y - 50);
+           
+            if (isplayerOn)
+            {
+                playerLight.SetActive(false);
+                isplayerOn = false;
+            }
+            else if(isenemyOn)
+            {
+                enemyLight.SetActive(false);
+                isenemyOn = false;
+            }
             //Debug.Log("This object is de-selected and its position is <" + transform.position + ">");
         }
        

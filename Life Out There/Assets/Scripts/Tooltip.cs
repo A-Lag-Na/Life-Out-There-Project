0cc052fd3f2b3bf4960d4932bb2788812afd1774
Alegna Medina <public.alegna.21@gmail.com>
2021-07-21 19:21:36 -0400

    Previous Commit comments forgot to add files

---
+++ Life Out There/Assets/Scripts/Tooltip.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Tooltip : MonoBehaviour
+{
+    private static Tooltip instance;
+
+    [SerializeField] private Camera uiCamera;
+
+    private TextMeshProUGUI tooltipText;
+    private RectTransform backgroundRectTransform;
+
+    private void Awake()
+    {
+        instance = this;
+        backgroundRectTransform = transform.Find("Background").GetComponent<RectTransform>();
+        tooltipText = transform.Find("Information").GetComponent<TextMeshProUGUI>();
+    }
+
+    private void Update()
+    {
+        transform.position = Input.mousePosition;
+    }
+
+    private void ShowTooltip(string tooltipString)
+    {
+        gameObject.SetActive(true);
+
+        tooltipText.SetText(tooltipString);
+        tooltipText.ForceMeshUpdate();
+        Vector2 textSize = tooltipText.GetRenderedValues(false);
+        backgroundRectTransform.sizeDelta = textSize;
+    }
+
+    private void HideTooltip()
+    {
+        gameObject.SetActive(false);
+    }
+
+    public static void ShowTooltip_Static(string tooltipString)
+    {
+        instance.ShowTooltip(tooltipString);
+    }
+
+    public static void HideTooltip_Static()
+    {
+        instance.HideTooltip();
+    }
+
+
+}

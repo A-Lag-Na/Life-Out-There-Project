dd3ac0e68f894ffd887df9754a05bb2e316b5467
Alegna Medina <public.alegna.21@gmail.com>
2021-08-12 14:05:59 -0400

    Map Intergrated

--- Life Out There/Assets/Scripts/Map Scripts/MapView.cs
+++ Life Out There/Assets/Scripts/Map Scripts/MapView.cs
@@ -225,7 +225,7 @@ namespace Map
             Debug.Log("Map span in set orientation: " + span + " camera aspect: " + cam.aspect);
 
             // setting first parent to be right in front of the camera first:
-            firstParent.transform.position = new Vector3(cam.transform.position.x, cam.transform.position.y, 20.0f);
+            firstParent.transform.position = new Vector3(cam.transform.position.x, cam.transform.position.y, 0f);
             var offset = orientationOffset;
             switch (orientation)
             {

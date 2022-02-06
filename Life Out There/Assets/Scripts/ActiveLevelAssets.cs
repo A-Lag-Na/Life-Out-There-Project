97d640fac0ba90aefa283625e688c6b2aeded4e0
Alegna Medina <public.alegna.21@gmail.com>
2022-02-06 18:48:35 -0500

    Made Background Prefabs for scenes and removed background from scene. Made A script to aide setting up the level assets at runtime.

---
+++ Life Out There/Assets/Scripts/ActiveLevelAssets.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActiveLevelAssets : MonoBehaviour
+{
+    [SerializeField]
+    string activeBackground;
+    [SerializeField]
+    string activeMusic;
+
+    void ActivateLevelAssets(GameObject backgroundPrefab, AudioSource levelmusic)
+    {
+        //Instantiate()
+    }
+
+}
